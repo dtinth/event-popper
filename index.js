9149c44bdf150f8b76d586eefe78261fb0ae50e2
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-16 17:29:33 +0700

    update for 2023

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // @ts-check
-const axios = require('axios')
-const cheerio = require('cheerio')
-const debug = require('debug')('event-popper')
-const { VError } = require('verror')
+const axios = require("axios");
+const cheerio = require("cheerio");
+const debug = require("debug")("event-popper");
+const { VError } = require("verror");
 
-exports.updateEventDescription = updateEventDescription
+exports.updateEventDescription = updateEventDescription;
 
 /**
  * @typedef {Object} EventPopConfig
@@ -21,39 +21,37 @@ async function updateEventDescription(html, config) {
   try {
     const client = axios.default.create({
       headers: {
-        cookie: `_stampmein_session=${config.sessionId}`
-      }
-    })
-    debug('Getting page editor')
-    const editorUrl = `https://www.eventpop.me/organizers/${
-      config.organizerId
-    }/events/${config.eventId}/edit/page_editor`
+        cookie: `_stampmein_session=${config.sessionId}`,
+      },
+    });
+    debug("Getting page editor");
+    const editorUrl = `https://www.eventpop.me/organizers/${config.organizerId}/event_details/${config.eventId}/edit/page_editor`;
 
     const getResponse = await client
-      .get(editorUrl, { responseType: 'text' })
-      .catch(e => {
-        throw new VError(e, 'Cannot GET page_editor')
-      })
-    const $ = cheerio.load(getResponse.data)
-    const token = $('input[name="authenticity_token"]').val()
+      .get(editorUrl, { responseType: "text" })
+      .catch((e) => {
+        throw new VError(e, "Cannot GET page_editor");
+      });
+    const $ = cheerio.load(getResponse.data);
+    const token = $('input[name="authenticity_token"]').val();
     if (!token) {
-      debug(getResponse.data)
-      throw new VError(`Authenticity token not found in page_editor response`)
+      debug(getResponse.data);
+      throw new VError(`Authenticity token not found in page_editor response`);
     }
-    debug('Found authenticity_token', token)
+    debug("Found authenticity_token", token);
 
     const patchResponse = await client
       .patch(editorUrl, {
         authenticity_token: token,
-        event: {
-          description: html
-        }
+        event_detail: {
+          description_th: html,
+        },
       })
-      .catch(e => {
-        throw new VError(e, 'Cannot PATCH page_editor')
-      })
-    debug('PATCH page_editor returned', patchResponse.status)
+      .catch((e) => {
+        throw new VError(e, "Cannot PATCH page_editor");
+      });
+    debug("PATCH page_editor returned", patchResponse.status);
   } catch (e) {
-    throw new VError(e, 'updateEventDescription failed')
+    throw new VError(e, "updateEventDescription failed");
   }
 }
